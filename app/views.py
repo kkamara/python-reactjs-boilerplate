8fc7485735b0699fd56f4d39aedf4f78eec4153f
kkamara <kamaracomputers@gmail.com>
2026-08-07 21:59:54 +0100

    Add rate-limiting to React homepage

--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
 from django.views.generic import TemplateView
+from django_ratelimit.decorators import ratelimit
 
-catchall = TemplateView.as_view(template_name='index.html')
+catchall = ratelimit(
+    key='ip', 
+    rate='40/m', 
+    method='GET'
+)(TemplateView.as_view(template_name='index.html'))
